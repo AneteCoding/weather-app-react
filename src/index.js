7b5ccb702d8622f30d6085faec044d8f6669d977
AneteCoding <77099964+AneteCoding@users.noreply.github.com>
2021-01-20 16:22:02 +0100

    fixing errors

--- src/index.js
+++ src/index.js
@@ -2,22 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import "./App.css";
 import reportWebVitals from './reportWebVitals';
-import Search from "./Search";
-import Forecast from "./Forecast";
+import App from "./App.js"
 
-function App() {
-  return (
-    <div className="App">
-      <div className="AppDesign">
-        <Search />
-        <Forecast />
-      </div>
-      <small>
-        <a href="https://github.com/AneteCoding/weather-app-react" target="_blank" rel="noreferrer">Open-source code</a>, by  <a href="https://www.linkedin.com/in/anete-bogdanova-a5ba366a/" target="_blank" rel="noreferrer">Anete</a>
-      </small>
-    </div>
-  );
-}
+
+
 
 ReactDOM.render(
   <React.StrictMode>
