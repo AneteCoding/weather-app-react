af5624fbd39d2de3e51e58138e9b995e8e1213ec
AneteCoding <77099964+AneteCoding@users.noreply.github.com>
2021-02-06 13:42:17 +0100

    adding current Location

--- src/Weather.js
+++ src/Weather.js
@@ -10,6 +10,7 @@ export default function Weather(props) {
     const [weather, setWeather] = useState({ loaded: false });
     const [city, setCity] = useState(props.defaultCity);
 
+
     function showWeather(response) {
         setWeather({
             loaded: true,
@@ -31,6 +32,21 @@ export default function Weather(props) {
     function handleChange(event) {
         setCity(event.target.value);
     }
+
+
+    function findLocation(event) {
+        event.preventDefault();
+        navigator.geolocation.getCurrentPosition(searchLocation);
+
+    }
+
+    function searchLocation(position) {
+        let apiKey = "e51c2a6a7756a9cab824e5d6224c7dcc";
+        let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+        axios.get(apiUrl).then(showWeather);
+
+    }
+
 
     function search() {
         let apiKey = "e51c2a6a7756a9cab824e5d6224c7dcc";
@@ -57,15 +73,16 @@ export default function Weather(props) {
                         <div className="col-2 search">
                             <input
                                 type="submit"
-                                class="btn"
+                                className="btn"
                                 value=" 🔍 "
                             />
                         </div>
                         <div className="col-2 location">
                             <input
-                                type="submit"
-                                class="btn"
+                                type="button"
+                                className="btn"
                                 value=" 📍 "
+                                onClick={findLocation}
                             />
 
                         </div>
